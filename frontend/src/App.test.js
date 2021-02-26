a8bcbc85739a6b8cba389de3219834ab1754e0b2
Duc Anh (Ryan) Nguyen <nduc.anh@gmail.com>
2021-02-26 08:29:54 +0700

    add travis for ci/cd (#63)
    * add travis for ci/cd
    
    * rename requirements.txt
    
    * modify folder tests
    
    * modify travis.yml
    
    * attempt 2 travis
    
    * TODO: remake requirements.txt
    
    * finish basic ci/cd
    
    * final cicd
    
    * sanity check
    
    * final
    
    * change to use pip3
    
    * add pytest.ini
    
    * fixed requirements.txt
    
    * pls work github
    
    * fix pytest
    
    * pls

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const sum = (a, b) => a + b;
+test("adds 1 + 2 to equal 3 - sanity check", () => {
+  expect(sum(1, 2)).toBe(3);
 });
