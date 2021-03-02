28da5751e34c63f8afd4f799a9021646c34514c5
Leroy Le <44736156+LeroyS1@users.noreply.github.com>
2021-03-02 01:09:02 -0800

    fixed bugs

--- backend/manage.py
+++ backend/manage.py
@@ -6,8 +6,9 @@ import django
 
 
 def main():
-    django.setup()
+    
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+    django.setup()
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
