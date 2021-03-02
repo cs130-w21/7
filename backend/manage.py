1a620e718730cc4f8ecd41157c5d8438784de61a
Leroy Le <44736156+LeroyS1@users.noreply.github.com>
2021-03-02 01:03:12 -0800

    Another Updates

--- backend/manage.py
+++ backend/manage.py
@@ -2,9 +2,11 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import django
 
 
 def main():
+    django.setup()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
     try:
         from django.core.management import execute_from_command_line
