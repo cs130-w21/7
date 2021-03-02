7ebd4099c7402daa748b9b09d078e3ca065822ec
Leroy Le <44736156+LeroyS1@users.noreply.github.com>
2021-03-02 00:45:44 -0800

    Update setting.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -149,3 +149,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
