2e69aff3573afbed983d24f2efa0b7c748d37acf
ngducn <nduc.anh@gmail.com>
2021-03-04 02:40:24 +0700

    final edit for first version

--- frontend/src/tests/ui-integration.test.js
+++ frontend/src/tests/ui-integration.test.js
@@ -3,11 +3,9 @@ import '@testing-library/jest-dom/extend-expect'
 import ReactDOM from 'react-dom';
 import { act } from 'react-dom/test-utils'
 import { unmountComponentAtNode } from 'react-dom';
-import Home from '../components/Pages/Home';
 import Create from '../components/Pages/Create';
 import LogIn from '../components/Pages/LogIn';
 import SignUp from '../components/Pages/SignUp';
-import Recommend from '../components/Pages/Recommend';
 
 
 let container = null;
@@ -22,16 +20,6 @@ afterEach(() => {
   unmountComponentAtNode(container);
   container.remove();
   container = null;
-});
-
-console.log('Running integration tests\n');
-
-it('integrates Home', () => {
-  act(() => {
-    ReactDOM.render(<Home />, container);
-  });
-
-  expect();
 });
 
 it('integrates Create', () => {
