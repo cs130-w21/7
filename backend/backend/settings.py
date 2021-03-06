715dc8e4be1dfef28a23af77bdf2741d35535315
Leroy Le <leroyle.ll@gmail.com>
2021-03-05 22:47:59 -0800

    fixed Procfile again 2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
 
 
 # REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend')
