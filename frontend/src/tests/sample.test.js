9915401804cf2480259c855a2f0974ee39c95660
ngducn <nduc.anh@gmail.com>
2021-03-06 11:29:42 +0700

    minor fix

--- frontend/src/tests/sample.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-const sum = (a, b) => a + b;
-test("adds 1 + 2 to equal 3 - sanity check", () => {
-  expect(sum(1, 2)).toBe(3);
-});
