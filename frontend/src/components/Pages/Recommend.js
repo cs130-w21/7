bc901f21a6fe0eeffc2d089cb46c4f741f3e9289
Leroy Le <44736156+LeroyS1@users.noreply.github.com>
2021-03-07 15:45:14 -0800

    Update Recommend Page

--- frontend/src/components/Pages/Recommend.js
+++ frontend/src/components/Pages/Recommend.js
@@ -5,6 +5,7 @@ import '../Cards.css';
 
 function Recommend() {
     const [isLoading, setIsLoading] = useState(false);
+    const [restaurants, setRestaurants] = useState({});
     
     var state = {
         listitem1: [
@@ -23,8 +24,6 @@ function Recommend() {
         var token = localStorage.getItem('token');
         const {coords} = await getUserLocation();
         const {latitude, longitude} = coords;
-        console.log(latitude)
-        console.log(longitude)
         
         const postLocation = async () => {
             setIsLoading(true);
@@ -38,6 +37,7 @@ function Recommend() {
             .then(response => response.json())
             .then(data => {
                 console.log(data)
+                setRestaurants(data)
                 setIsLoading(false);
             })
         }
@@ -46,51 +46,31 @@ function Recommend() {
 
 
         
-    })
-
+    },[])
     
     //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyCY4-DiMtofTkDmGbLyL01OyXfdh1S0wBc
     //https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=ATtYBwKwwOlknJALhji-kYQzZHV0c3v7quYsrRU5b6Ci2qDuezSVvpHfSA59ZxaREMLUoKcaG7sOCHrxhdgp0bp0nCOAGnhDNpCVF5XV0hcOxE97WzyX3kMUkemmGmoaQopJuPnm7SRqKuNKQzDvCr0uGcWcpN1yRS3VmbZ4Z4Kzhkq8xx_r&sensor=false&key=AIzaSyCY4-DiMtofTkDmGbLyL01OyXfdh1S0wBc
     return (
         <div className='cards'>
-            <h1>Here are some suggestions!</h1>
+            {/* <h1>Here are some suggestions!</h1>
             <div className='cards__container'>
             <div className='cards__wrapper'>
                 <ul id='listview' className='cards__items'>
                 {
-                state.listitem1.map(listitem1 => (
+                    
+                restaurants.map(li => (
                     <CardView
-                    src={listitem1.src}
-                    key={listitem1.text}
-                    text={listitem1.text}
-                    label={listitem1.label}
-                    path={listitem1.path}
+                    src={li.img_url}
+                    key={li.alias}
+                    text={li.location.display_address}
+                    label={li.categories.alias}
+                    path={li.url}
                     />
                 ))}
                 </ul>
-                <ul className='cards__items'>
-                <CardView
-                    src='images/img-800.jpg'
-                    text='800 Degrees Pizza : 10889 Lindbrook Dr, Los Angeles, CA 90024
-                    '
-                    label='Pizza'
-                    path='/services'
-                />
-                <CardView
-                    src='images/img-le-pain-quotidien.jpeg'
-                    text='Le Pain Quotidien : 1122 Gayley Ave, Los Angeles, CA 90024'
-                    label='Italian'
-                    path='/services'
-                />
-                <CardView
-                    src='images/img-napa-valley-grille.jpeg'
-                    text='Napa Valley Grille : 1100 Glendon Ave Ste 100, Los Angeles, CA 90024'
-                    label='Steak'
-                    path='/services'
-                />
-                </ul>
+            
             </div>
-            </div>
+            </div> */}
         </div>
     );
 }
