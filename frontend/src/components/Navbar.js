ba8ddedd7257c682a08e805f4d5146aadaa7325f
Leroy Le <44736156+LeroyS1@users.noreply.github.com>
2021-03-07 19:09:41 -0800

    finish setting page

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -82,11 +82,11 @@ function Navbar() {
                                 Events
                             </Link>
                         </li>
-                        <li className='nav-item'>
+                        {/* <li className='nav-item'>
                             <Link to='/create' className='nav-links' onClick={closeMobileMenu}>
                                 Create
                             </Link>
-                        </li>
+                        </li> */}
                         <li className='nav-item'>
                             <Link to='/setting' className='nav-links' onClick={closeMobileMenu}>
                                 Profile
